15db2e966d403ec75658d68a51acca18a9d47c28
skyblue813 <pthoppae@emich.edu>
2021-10-01 21:45:18 +0000

    added inline style to components

--- src/Components/courses.js
+++ src/Components/courses.js
@@ -1,9 +1,8 @@
-
-
+import '/home/ec2-user/pt_website/profile-app-skyblue813/src/App.js'
 function Courses() {
   return (
-    <div>
-        <h2>Courses</h2>
+    <div style={{backgroundColor:'#BDEDFF'}}>
+        <h2 style={{backgroundColor:'#98AFC7'}}>Courses</h2>
       <ul>
           <li>Artificial Intelligence</li>
           <li>Game Design and Development</li>
