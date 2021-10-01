a6d29d1812c2dd6c9dcaddd58c6725288f479dac
skyblue813 <pthoppae@emich.edu>
2021-10-01 20:13:11 +0000

    added contact component

---
+++ src/Components/contact.js
@@ -0,0 +1,13 @@
+import './App.css';
+function Contact() {
+    return (
+      <div>
+        <p>pthoppae@emich.edu<br/>
+           Ph: (313)-123-4567<br/>
+           Ann Arbor, MI
+        </p>
+      </div>
+    );
+  }
+  
+  export default Contact;
