703dea24776442d3464640b5bd30b0cd06283fb7
skyblue813 <pthoppae@emich.edu>
2021-10-09 03:31:10 +0000

    Added Module 1 and 2

--- src/App.js
+++ src/App.js
@@ -1,3 +1,7 @@
+import { BrowserRouter,Route } from "react-router-dom";
+import Module1 from "./Components/Module1";
+import Module2 from "./Components/Module2";
+
 
 
 
@@ -6,6 +10,15 @@ function App() {
     <div>
       <header>
         <h1>Priyadarshini G Thoppae</h1>
+        <BrowserRouter>
+           <Route path ="/Module1">
+            <Module1/>
+            </Route>
+
+            <Route path ="/Module2">
+        <Module2/>
+            </Route >
+        </BrowserRouter>
       </header>
     </div>
   );
---
+++ src/Components/Module1.js
@@ -0,0 +1,10 @@
+function Module1() {
+    return (
+      <div>
+        <h3>Profile Pic</h3>
+        <img src="https://i.guim.co.uk/img/media/26392d05302e02f7bf4eb143bb84c8097d09144b/446_167_3683_2210/master/3683.jpg?width=1200&height=1200&quality=85&auto=format&fit=crop&s=49ed3252c0b2ffb49cf8b508892e452d"></img>
+      </div>
+    );
+  }
+  
+  export default Module1;
